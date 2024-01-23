b0a71e61dbce73c9a44b9fee05cd3d1a471fb869
haechan29 <haechan.im@gmail.com>
2024-01-23 11:10:47 +0900

    refactor functions

---
+++ app/src/main/java/com/hc/iphone_likeclock/FlingBehaviorWithOnFinished.kt
@@ -0,0 +1,39 @@
+package com.hc.iphone_likeclock
+
+import androidx.compose.animation.core.AnimationState
+import androidx.compose.animation.core.DecayAnimationSpec
+import androidx.compose.animation.core.animateDecay
+import androidx.compose.foundation.gestures.FlingBehavior
+import androidx.compose.foundation.gestures.ScrollScope
+import kotlinx.coroutines.Job
+import kotlin.math.abs
+
+internal class FlingBehaviorWithOnFinished(
+    private val decayAnimSpec: DecayAnimationSpec<Float>,
+    private val onFinished: () -> Job
+) : FlingBehavior {
+    override suspend fun ScrollScope.performFling(initialVelocity: Float): Float {
+        var isAnimationRunning = true
+        var velocityLeft = initialVelocity
+        var lastValue = 0f
+        val animationState = AnimationState(
+            initialValue = 0f,
+            initialVelocity = initialVelocity,
+        )
+        animationState.animateDecay(decayAnimSpec) {
+            val delta = value - lastValue
+            val consumed = scrollBy(delta)
+            lastValue = value
+            velocityLeft = this.velocity
+            if (isAnimationRunning != isRunning) {
+                if (!isRunning) {
+                    onFinished()
+                }
+                isAnimationRunning = isRunning
+            }
+            // avoid rounding errors and stop if anything is unconsumed
+            if (abs(delta - consumed) > 0.5f) this.cancelAnimation()
+        }
+        return velocityLeft
+    }
+}
--- app/src/main/java/com/hc/iphone_likeclock/MainActivity.kt
+++ app/src/main/java/com/hc/iphone_likeclock/MainActivity.kt
@@ -2,17 +2,11 @@ package com.hc.iphone_likeclock
 
 import android.content.res.Resources
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.AnimationState
-import androidx.compose.animation.core.DecayAnimationSpec
-import androidx.compose.animation.core.animateDecay
 import androidx.compose.animation.rememberSplineBasedDecay
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.border
 import androidx.compose.foundation.gestures.FlingBehavior
-import androidx.compose.foundation.gestures.ScrollScope
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -20,7 +14,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListItemInfo
 import androidx.compose.foundation.lazy.LazyListLayoutInfo
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -39,6 +35,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.hc.iphone_likeclock.MainActivity.Companion.CLOCK_RADIUS
 import com.hc.iphone_likeclock.MainActivity.Companion.DEGREE_OF_ENDPOINT
+import com.hc.iphone_likeclock.MainActivity.Companion.DEGREE_VISIBLE
 import com.hc.iphone_likeclock.MainActivity.Companion.ITEM_SIZE
 import com.hc.iphone_likeclock.ui.theme.IPhoneLikeClockTheme
 import kotlinx.coroutines.Job
@@ -60,6 +57,7 @@ class MainActivity : ComponentActivity() {
     }
 
     companion object {
+        const val DEGREE_VISIBLE = PI / 3
         const val DEGREE_OF_ENDPOINT = PI / 2.6
         const val CLOCK_RADIUS = 120
         const val ITEM_SIZE = 50
@@ -94,111 +92,67 @@ fun IPhoneLikeClock() {
     }
 }
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun Clock(items: List<String>) {
     val listState = rememberLazyListState()
-    val height = CLOCK_RADIUS.dp * sin(PI / 3).toFloat() * 2
     val scope = rememberCoroutineScope()
-    val firstVisibleItemIndex by remember { derivedStateOf { listState.firstVisibleItemIndex } }
     val layoutInfo by remember { derivedStateOf { listState.layoutInfo } }
-    val centerItemIndex by remember {
-        derivedStateOf {
-            val visibleItems = layoutInfo.visibleItemsInfo
-            val viewportCenter = (layoutInfo.viewportStartOffset + layoutInfo.viewportEndOffset) / 2
-            visibleItems.minByOrNull { abs((it.offset + it.size / 2) - viewportCenter) }?.index ?: 0
-        }
-    }
+    val centerItemIndex by remember { derivedStateOf { getItemAtCenter(layoutInfo)?.index ?: 0 } }
     LazyColumn(
         modifier = Modifier
             .fillMaxWidth()
-            .height(height),
+            .height(calcClockHeight()),
         state = listState,
-        flingBehavior = maxScrollFlingBehavior {
+        flingBehavior = flingBehaviorWithOnFinished {
             scope.launch {
                 listState.animateScrollToItem(centerItemIndex - 2)
             }
         }
     ) {
         items(items.size) { itemIndex ->
-            val indexInVisibleItems = itemIndex - firstVisibleItemIndex
-            Text(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(
-                        if (!isItemVisible(layoutInfo, indexInVisibleItems))
-                            calcItemHeight(ITEM_SIZE.dp, DEGREE_OF_ENDPOINT)
-                        else {
-                            val degree = getRotationDegree(layoutInfo, indexInVisibleItems)
-                            calcItemHeight(ITEM_SIZE.dp, degree)
-                        }
-                    )
-                    .graphicsLayer {
-                        if (!isItemVisible(layoutInfo, indexInVisibleItems)) return@graphicsLayer
-                        rotationX = getRotationDegree(layoutInfo, indexInVisibleItems).toDegree().toFloat()
-                    }
-                    .wrapContentHeight(CenterVertically),
-                text = items[itemIndex],
-                fontSize = 24.sp,
-                textAlign = TextAlign.Center
-            )
+            RotatingText(items, itemIndex, listState, layoutInfo)
         }
     }
-}
-
-fun getRotationDegree(layoutInfo: LazyListLayoutInfo, indexInVisibleItems: Int): Double {
-    val viewportHeight = with(layoutInfo) { viewportEndOffset - viewportStartOffset }
-    val itemInfo = layoutInfo.visibleItemsInfo[indexInVisibleItems]
-    val itemCenterOffset = itemInfo.offset + itemInfo.size / 2
-    val h = viewportHeight / 2 - itemCenterOffset
-    val r = CLOCK_RADIUS.toPx()
-    return asin(h.toDouble() / r.toDouble())
 }
 
 @Composable
-fun maxScrollFlingBehavior(function: () -> Job): FlingBehavior {
-    val flingSpec = rememberSplineBasedDecay<Float>()
-    return remember(flingSpec) {
-        ScrollSpeedFlingBehavior(flingSpec, function)
-    }
-}
-
-private class ScrollSpeedFlingBehavior(
-    private val flingDecay: DecayAnimationSpec<Float>,
-    val function: () -> Job
-) : FlingBehavior {
-    override suspend fun ScrollScope.performFling(initialVelocity: Float): Float {
-        Log.d("hhcc", "fling start with velocity: $initialVelocity")
-        var isAnimationRunning = true
-        var velocityLeft = initialVelocity
-        var lastValue = 0f
-        val animationState = AnimationState(
-            initialValue = 0f,
-            initialVelocity = initialVelocity,
-        )
-        animationState.animateDecay(flingDecay) {
-            Log.d("hhcc", "isRunning: $isRunning")
-            val delta = value - lastValue
-            val consumed = scrollBy(delta)
-            lastValue = value
-            velocityLeft = this.velocity
-            if (isAnimationRunning != isRunning) {
-                if (!isRunning) {
-                    Log.d("hhcc", "fling stopped")
-                    function()
+fun RotatingText(
+    items: List<String>,
+    itemIndex: Int,
+    listState: LazyListState,
+    layoutInfo: LazyListLayoutInfo
+) {
+    val firstVisibleItemIndex by remember { derivedStateOf { listState.firstVisibleItemIndex } }
+    val indexInVisibleItems = itemIndex - firstVisibleItemIndex
+    Text(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(
+                if (!isItemVisible(layoutInfo, indexInVisibleItems))
+                    calcItemHeight(ITEM_SIZE.dp, DEGREE_OF_ENDPOINT)
+                else {
+                    val degree = getRotationDegree(layoutInfo, indexInVisibleItems)
+                    calcItemHeight(ITEM_SIZE.dp, degree)
                 }
-                isAnimationRunning = isRunning
+            )
+            .graphicsLayer {
+                if (!isItemVisible(layoutInfo, indexInVisibleItems)) return@graphicsLayer
+                rotationX = getRotationDegree(layoutInfo, indexInVisibleItems).toDegree().toFloat()
             }
-            // avoid rounding errors and stop if anything is unconsumed
-            if (abs(delta - consumed) > 0.5f) this.cancelAnimation()
-        }
-        return velocityLeft
-    }
+            .wrapContentHeight(CenterVertically),
+        text = items[itemIndex],
+        fontSize = 24.sp,
+        textAlign = TextAlign.Center
+    )
 }
 
-fun isItemVisible(layoutInfo: LazyListLayoutInfo, i: Int) = i in layoutInfo.visibleItemsInfo.indices
-fun calcItemHeight(itemSize: Dp, degree: Double) = itemSize * cos(degree).toFloat()
-
+@Composable
+fun flingBehaviorWithOnFinished(onFinished: () -> Job): FlingBehavior {
+    val decayAnimSpec = rememberSplineBasedDecay<Float>()
+    return remember(decayAnimSpec) {
+        FlingBehaviorWithOnFinished(decayAnimSpec, onFinished)
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
@@ -209,7 +163,26 @@ fun ClockPreview() {
 }
 
 private fun List<String>.addedEmptySpaces() = listOf(listOf("", ""), this, listOf("", "")).flatten()
+fun calcClockHeight() = CLOCK_RADIUS.dp * sin(DEGREE_VISIBLE).toFloat() * 2
 
+fun getItemAtCenter(layoutInfo: LazyListLayoutInfo): LazyListItemInfo? {
+    val viewportCenterOffset = (layoutInfo.viewportStartOffset + layoutInfo.viewportEndOffset) / 2
+    return layoutInfo.visibleItemsInfo.minByOrNull { abs(calcItemCenterOffset(it) - viewportCenterOffset) }
+}
+
+fun isItemVisible(layoutInfo: LazyListLayoutInfo, i: Int) = i in layoutInfo.visibleItemsInfo.indices
+fun calcItemHeight(itemSize: Dp, degree: Double) = itemSize * cos(degree).toFloat()
+
+fun getRotationDegree(layoutInfo: LazyListLayoutInfo, indexInVisibleItems: Int): Double {
+    val viewportHeight = with(layoutInfo) { viewportEndOffset - viewportStartOffset }
+    val itemInfo = layoutInfo.visibleItemsInfo[indexInVisibleItems]
+    val itemCenterOffset = calcItemCenterOffset(itemInfo)
+    val h = viewportHeight / 2 - itemCenterOffset
+    val r = CLOCK_RADIUS.toPx()
+    return asin(h.toDouble() / r.toDouble())
+}
+
+fun calcItemCenterOffset(itemInfo: LazyListItemInfo) = itemInfo.offset + itemInfo.size / 2
 fun Double.toRadian() = this / 180f * PI
 fun Double.toDegree() = this * 180 / PI
 fun Int.toDp() = (this / Resources.getSystem().displayMetrics.density).toInt().dp
