39d6ef54516bb02ee3b98873d2aa402226aeeaac
haechan29 <haechan.im@gmail.com>
2024-01-24 13:14:48 +0900

    add TranslucentScreen

--- app/src/main/java/com/hc/iphone_likeclock/MainActivity.kt
+++ app/src/main/java/com/hc/iphone_likeclock/MainActivity.kt
@@ -5,35 +5,49 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.rememberSplineBasedDecay
-import androidx.compose.foundation.border
+import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.FlingBehavior
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListItemInfo
 import androidx.compose.foundation.lazy.LazyListLayoutInfo
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.BottomCenter
+import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.Alignment.Companion.TopCenter
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.hc.iphone_likeclock.MainActivity.Companion.CLOCK_RADIUS
+import com.hc.iphone_likeclock.MainActivity.Companion.CLOCK_WIDTH
 import com.hc.iphone_likeclock.MainActivity.Companion.DEGREE_OF_ENDPOINT
 import com.hc.iphone_likeclock.MainActivity.Companion.DEGREE_VISIBLE
 import com.hc.iphone_likeclock.MainActivity.Companion.ITEM_SIZE
@@ -59,6 +73,7 @@ class MainActivity : ComponentActivity() {
     companion object {
         const val DEGREE_VISIBLE = PI / 3
         const val DEGREE_OF_ENDPOINT = PI / 2.6
+        const val CLOCK_WIDTH = 300
         const val CLOCK_RADIUS = 120
         const val ITEM_SIZE = 50
     }
@@ -67,29 +82,89 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun IPhoneLikeClock() {
     val periods = listOf("오전", "오후").addedEmptySpaces()
-    val hours = (1 .. 12).map { it.toString() }.toList().addedEmptySpaces()
-    val minutes = (1 .. 59).map { it.toString() }.toList().addedEmptySpaces()
-    Column {
-        Box(modifier = Modifier
+    val hours = (1..12).map { it.toString() }.toList().addedEmptySpaces()
+    val minutes = (1..59).map { it.toString() }.toList().addedEmptySpaces()
+    Box(
+        modifier = Modifier
+            .padding(top = 100.dp, bottom = 100.dp)
             .fillMaxWidth()
-            .height(100.dp)
-        )
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .border(1.dp, Color.Black)
+            .height(calcClockHeight()),
+        contentAlignment = Center
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-//            Box(modifier = Modifier.weight(1f)) {
-//                Clock(120.dp, 50.dp, periods)
-//            }
-//            Box(modifier = Modifier.weight(1f)) {
-//                Clock(120.dp, 50.dp, hours)
-//            }
-            Box(modifier = Modifier.weight(1f)) {
-                Clock(minutes)
+            Row(modifier = Modifier.width(CLOCK_WIDTH.dp)) {
+                Box(modifier = Modifier.weight(1f)) {
+                    Clock(periods)
+                }
+                Box(modifier = Modifier.weight(1f)) {
+                    Clock(hours)
+                }
+                Box(modifier = Modifier.weight(1f)) {
+                    Clock(minutes)
+                }
             }
         }
+        TranslucentScreen()
     }
+}
+
+@Composable
+fun BoxScope.TranslucentScreen() {
+    Box(
+        modifier = Modifier.fillMaxWidth()
+            .height((calcClockHeight() - ITEM_SIZE.dp) / 2)
+            .alpha(0.05f)
+            .background(color = Color.Black)
+            .align(TopCenter)
+    )
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(ITEM_SIZE.dp)
+    ) {
+        val screenWidth = LocalConfiguration.current.screenWidthDp.dp
+        Box(
+            modifier = Modifier
+                .fillMaxHeight()
+                .width((screenWidth - CLOCK_WIDTH.dp) / 2)
+                .alpha(0.05f)
+                .background(color = Color.Black),
+            contentAlignment = Alignment.CenterEnd
+        ) {
+            Box(
+                modifier = Modifier
+                    .padding(start = 20.dp)
+                    .fillMaxSize()
+                    .background(color = Color.White, shape = RoundedCornerShape(10.dp, 0.dp, 0.dp, 10.dp))
+            )
+        }
+        Spacer(modifier = Modifier.weight(1f))
+        Box(
+            modifier = Modifier
+                .fillMaxHeight()
+                .width((screenWidth - CLOCK_WIDTH.dp) / 2)
+                .alpha(0.05f)
+                .background(color = Color.Black),
+            contentAlignment = Alignment.CenterStart
+        ) {
+            Box(
+                modifier = Modifier
+                    .padding(end = 20.dp)
+                    .fillMaxSize()
+                    .background(color = Color.White, shape = RoundedCornerShape(0.dp, 10.dp, 10.dp, 0.dp))
+            )
+        }
+    }
+    Box(
+        modifier = Modifier.fillMaxWidth()
+            .height((calcClockHeight() - ITEM_SIZE.dp) / 2)
+            .alpha(0.05f)
+            .background(color = Color.Black)
+            .align(BottomCenter)
+    )
 }
 
 @Composable
@@ -98,17 +173,15 @@ fun Clock(items: List<String>) {
     val scope = rememberCoroutineScope()
     val layoutInfo by remember { derivedStateOf { listState.layoutInfo } }
     val centerItemIndex by remember { derivedStateOf { getItemAtCenter(layoutInfo)?.index ?: 0 } }
-    LazyColumn(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(calcClockHeight()),
+    LazyColumn(modifier = Modifier
+        .fillMaxWidth()
+        .height(calcClockHeight()),
         state = listState,
         flingBehavior = flingBehaviorWithOnFinished {
             scope.launch {
                 listState.animateScrollToItem(centerItemIndex - 2)
             }
-        }
-    ) {
+        }) {
         items(items.size) { itemIndex ->
             RotatingText(items, itemIndex, listState, layoutInfo)
         }
@@ -117,10 +190,7 @@ fun Clock(items: List<String>) {
 
 @Composable
 fun RotatingText(
-    items: List<String>,
-    itemIndex: Int,
-    listState: LazyListState,
-    layoutInfo: LazyListLayoutInfo
+    items: List<String>, itemIndex: Int, listState: LazyListState, layoutInfo: LazyListLayoutInfo
 ) {
     val firstVisibleItemIndex by remember { derivedStateOf { listState.firstVisibleItemIndex } }
     val indexInVisibleItems = itemIndex - firstVisibleItemIndex
@@ -128,8 +198,9 @@ fun RotatingText(
         modifier = Modifier
             .fillMaxWidth()
             .height(
-                if (!isItemVisible(layoutInfo, indexInVisibleItems))
-                    calcItemHeight(ITEM_SIZE.dp, DEGREE_OF_ENDPOINT)
+                if (!isItemVisible(layoutInfo, indexInVisibleItems)) calcItemHeight(
+                    ITEM_SIZE.dp, DEGREE_OF_ENDPOINT
+                )
                 else {
                     val degree = getRotationDegree(layoutInfo, indexInVisibleItems)
                     calcItemHeight(ITEM_SIZE.dp, degree)
@@ -137,7 +208,9 @@ fun RotatingText(
             )
             .graphicsLayer {
                 if (!isItemVisible(layoutInfo, indexInVisibleItems)) return@graphicsLayer
-                rotationX = getRotationDegree(layoutInfo, indexInVisibleItems).toDegree().toFloat()
+                rotationX = getRotationDegree(layoutInfo, indexInVisibleItems)
+                    .toDegree()
+                    .toFloat()
             }
             .wrapContentHeight(CenterVertically),
         text = items[itemIndex],
@@ -158,7 +231,7 @@ fun flingBehaviorWithOnFinished(onFinished: () -> Job): FlingBehavior {
 @Composable
 fun ClockPreview() {
     IPhoneLikeClockTheme {
-        Clock((0 .. 100).map { it.toString() }.toList())
+        IPhoneLikeClock()
     }
 }
 
@@ -182,8 +255,8 @@ fun getRotationDegree(layoutInfo: LazyListLayoutInfo, indexInVisibleItems: Int):
     return asin(h.toDouble() / r.toDouble())
 }
 
-fun calcItemCenterOffset(itemInfo: LazyListItemInfo) = itemInfo.offset + itemInfo.size / 2
+fun calcItemCenterOffset(itemInfo: LazyListItemInfo) = with(itemInfo) { offset + size / 2 }
 fun Double.toRadian() = this / 180f * PI
 fun Double.toDegree() = this * 180 / PI
-fun Int.toDp() = (this / Resources.getSystem().displayMetrics.density).toInt().dp
+fun Int.toDp() = (this / Resources.getSystem().displayMetrics.density).toInt()
 fun Int.toPx() = (this * Resources.getSystem().displayMetrics.density).toInt()
